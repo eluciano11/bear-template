d17c8e4047c0ef2b87b2c6ad9565da63cfb45b04
Emmanuel Luciano <emma.luciano11@gmail.com>
2019-11-26 07:26:01 -0400

    Initial commit

---
+++ _includes/tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: '#FF9E0B',
+      black: '#000000',
+      gray: {
+        lighter: '#A3ABB3',
+        default: '#555',
+        dark: '#444'
+      },
+      white: {
+        default: '#FFFFF',
+        dark: '#FBFBFB'
+      }
+    },
+    spacing: {
+      px: '1px',
+      '0': '0',
+      '1': '4px',
+      '2': '8px',
+      '3': '12px',
+      '4': '16px',
+      '5': '20px',
+      '6': '24px',
+      '7': '28px',
+      '8': '32px',
+      '9': '36px',
+      '10': '40px',
+      '11': '44px',
+      '12': '48px',
+      '13': '52px',
+      '15': '56px',
+      '16': '60px',
+      '18': '64px',
+      '20': '80px'
+    }
+  },
+  variants: {},
+  plugins: []
+};
